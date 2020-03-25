e1788469272096f7a6f3349cf7ae5f2731834d73
piper <piper@dev0.sh>
2020-03-25 17:51:54 -0400

    functional file, v1

--- PingCheck.ps1
+++ PingCheck.ps1
@@ -3,11 +3,12 @@
 <#
 
 .SYNOPSIS
-This is a powerhsell script for logging speed and latenvy over an indefinite period of time when ran in the foreground. It uses a remote server (1.1.1.1 by default) and an interval (10 seconds) by default
+This is a powerhsell script for logging speed and latency over an indefinite period of time when ran in the foreground. It uses a remote server (1.1.1.1 by default) and an interval (10 seconds) by default
 
 .DESCRIPTION
 -remote <remote server IP or name>
 -interval <interval in seconds>
+-speedtest ($true|$false)
 
 .EXAMPLE
 ./PingCheck.ps1 -remote dev0.sh -interval 5
@@ -24,15 +25,18 @@ https://git.dev0.sh/piper/techsupport_scripts
 param ([string]$remote='1.1.1.1', [decimal]$interval='2', [bool]$speedtest=$false)
 $gate = $($(Get-NetIPConfiguration).IPv4DefaultGateway).NextHop
 
+# make our file
+Add-Content -Path $env:USERPROFILE\Desktop\PingCheck_$interval.csv  -Value "Time,$gate,$remote,Speed"
+
 While ($true) {
-	Write-Output "--------------------"
-	Get-Date -Format "%M/%d %H:%m:%s"
-	Test-Connection -ComputerName $remote -Count 1 | Format-Table Address,ResponseTime
-	Test-Connection -ComputerName $gate -Count 1 | Format-Table Address,ResponseTime
+	$time = Get-Date -Format "%M/%d %H:%m:%s"
+	$timeRemote = $(Test-Connection -ComputerName $remote -Count 1).ResponseTime
+	$timeGate = $(Test-Connection -ComputerName $gate -Count 1).ResponseTime
 	If ($speedtest){
-			Write-Output "Speed is $($a=Get-Date; Invoke-WebRequest https://dev0.sh/1MiB |Out-Null; "$((10/((Get-Date)-$a).TotalSeconds)*8) Mbps")"
+			$speed = $($a=Get-Date; Invoke-WebRequest https://dev0.sh/1MiB |Out-Null; "$((10/((Get-Date)-$a).TotalSeconds)*8) Mbps")
 		}Else{
-			Write-Output 'no speedtest requested'
+			$speed = "skipped"
 	}
+	Add-Content -Path $env:USERPROFILE\Desktop\PingCheck_$interval.csv -Value "$time,$timeGate,$timeRemote,$speed"
 	Start-Sleep -Seconds $interval
 }
