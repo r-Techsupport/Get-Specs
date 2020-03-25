b746fa240b0b871f02bf3f207c32b58a9e9bd9ed
piper <piper@dev0.sh>
2020-03-25 17:07:56 -0400

    add gateway and make speedtest optional

--- PingCheck.ps1
+++ PingCheck.ps1
@@ -20,12 +20,19 @@ https://git.dev0.sh/piper/techsupport_scripts
 
 #>
 
-param ([string]$remote='1.1.1.1', [decimal]$interval='10')
+#get things
+param ([string]$remote='1.1.1.1', [decimal]$interval='2', [bool]$speedtest=$false)
+$gate = $($(Get-NetIPConfiguration).IPv4DefaultGateway).NextHop
 
 While ($true) {
 	Write-Output "--------------------"
 	Get-Date -Format "%M/%d %H:%m:%s"
 	Test-Connection -ComputerName $remote -Count 1 | Format-Table Address,ResponseTime
-	Write-Output "Speed is $($a=Get-Date; Invoke-WebRequest https://dev0.sh/1MiB |Out-Null; "$((10/((Get-Date)-$a).TotalSeconds)*8) Mbps")"
+	Test-Connection -ComputerName $gate -Count 1 | Format-Table Address,ResponseTime
+	If ($speedtest){
+			Write-Output "Speed is $($a=Get-Date; Invoke-WebRequest https://dev0.sh/1MiB |Out-Null; "$((10/((Get-Date)-$a).TotalSeconds)*8) Mbps")"
+		}Else{
+			Write-Output 'no speedtest requested'
+	}
 	Start-Sleep -Seconds $interval
 }
