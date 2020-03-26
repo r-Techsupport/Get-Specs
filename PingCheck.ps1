1244e06ceb00a1dde262189247b6467e9ce2fca2
piper <piper@dev0.sh>
2020-03-26 09:41:01 -0400

    add twitter and yt, refine jobs and warnings

--- PingCheck.ps1
+++ PingCheck.ps1
@@ -14,7 +14,7 @@ This is a powerhsell script for logging speed and latency over an indefinite per
 ./PingCheck.ps1 -remote dev0.sh -interval 5
 
 .NOTES
-No notes at the moment
+If you run a speedtest your minimum effective interval is the duration of the speedtest +1s
 
 .LINK
 https://git.dev0.sh/piper/techsupport_scripts
@@ -26,15 +26,19 @@ param ([string]$remote='1.1.1.1', [decimal]$interval='2', [bool]$speedtest=$fals
 $gate = $($(Get-NetIPConfiguration).IPv4DefaultGateway).NextHop
 
 # make our file
-Add-Content -Path $env:USERPROFILE\Desktop\PingCheck_$interval.csv  -Value "Time,$gate,$remote,Speed"
+$csv = "$env:USERPROFILE\Desktop\PingCheck_$interval.csv"
+Add-Content -Path $csv  -Value "Time,$gate,$remote,Youtube,Twitter,Speed"
 
-Write-Host "All output is being made in $env:USERPROFILE\Desktop\PingCheck_$interval.csv" 
+Write-Host "All output is being made in $csv" -Foreground Green
+Write-Warning "If you run a speedtest your minimum effective interval is the duration of the speedtest +1s"
 Write-Warning "Do not open the file until you have terminated this script."
 
 While ($true) {
 	$time = Get-Date -Format "%M/%d %H:%m:%s"
 	$gateJob = Start-Job { $(Test-Connection -ComputerName $using:gate -Count 1).ResponseTime }
 	$remoteJob = Start-Job { $(Test-Connection -ComputerName $using:remote -Count 1).ResponseTime }
+	$ytJob = Start-Job { $(Test-Connection -Computername 'youtube.com' -Count 1).ResponseTime }
+	$twJob = Start-Job { $(Test-Connection -Computername 'twitter.com' -Count 1).ResponseTime }
 	If ($speedtest){
 			$speed = $($a=Get-Date; Invoke-WebRequest https://dev0.sh/1MiB |Out-Null; "$((10/((Get-Date)-$a).TotalSeconds)*8) Mbps")
 		}Else{
@@ -43,6 +47,8 @@ While ($true) {
 	Start-Sleep '1'
 	$timeGate = Receive-Job $gateJob
 	$timeRemote = Receive-Job $remoteJob
-	Add-Content -Path $env:USERPROFILE\Desktop\PingCheck_$interval.csv -Value "$time,$timeGate ms,$timeRemote ms,$speed"
+	$timeYT = Receive-Job $ytJob
+	$timeTW = Receive-Job $twJob
+	Add-Content -Path $csv -Value "$time,$timeGate ms,$timeRemote ms,$timeYT ms,$timeTW ms,$speed"
 	Start-Sleep -Seconds $interval
 }
