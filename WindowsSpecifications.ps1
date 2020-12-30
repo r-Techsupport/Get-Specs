0e287afc5034cb35b058981b542fdb0abe7d0d99
piper <piper@dev0.sh>
2020-12-29 21:42:47 -0500

    .\WindowsSpecifications.ps1
    transition to exe compatible format

--- WindowsSpecifications.ps1
+++ WindowsSpecifications.ps1
@@ -12,119 +12,135 @@
   Purpose/Change: Initial script development
 #>
 
-#Start recording output
-Start-Transcript "TechSupport_Specs.txt"
-Get-Date
-Write-Host "`n" -NoNewline
+# ------------------ #
+$file = 'TechSupport_Specs.txt'
+# ------------------ #
 
-#Pull basic OS information
-Write-Host "Edition:" -NoNewline $(Get-Item "HKLM:Software\Microsoft\Windows NT\CurrentVersion").GetValue("ProductName")
-Write-Host "Build:" -NoNewline $(Get-Item "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion").GetValue("ReleaseID")
-Write-Host "`n" -NoNewline
-
-#Define function to get CPU model
-function Get-CPU{
+function startItOut {
+	Get-Date
+}
+function basicInfo {
+	$1 = 'Edition: ' + $(Get-Item "HKLM:Software\Microsoft\Windows NT\CurrentVersion").GetValue("ProductName")
+	$2 = 'Build: ' + $(Get-Item "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion").GetValue("ReleaseID")
+	Return $1,$2
+}
+function getCPU{
     $CPUInfo = Get-WmiObject Win32_Processor
     $CPU = $CPUInfo.Name
-    return $CPU
+	$1 = "`n" + 'CPU: ' + $CPU
+	Return $1
 }
-Write-Host "CPU: " -NoNewline
-Get-CPU
-Write-Host "`n" -NoNewline
-
-#Get RAM info
-Write-Host "RAM: "(Get-WMIObject -class Win32_PhysicalMemory | Measure-Object -Property capacity -Sum | % {[Math]::Round(($_.sum / 1GB),2)})GB -NoNewline
-Get-WmiObject win32_physicalmemory | Format-Table Manufacturer,Configuredclockspeed,Devicelocator,Capacity,Serialnumber -autosize
-Write-Host "`n" -NoNewline
-
-#Define function to get motherboard model
-function Get-Mobo{
+function getMobo{
     $moboBase = Get-WmiObject Win32_BaseBoard
     $moboMan = $moboBase.manufacturer
     $moboMod = $moboBase.product
     $mobo = $moboMan + " | " + $moboMod
-    return $mobo
+    $1 = "Motherboard: " + $mobo
+	Return $1
 }
-Write-Host "Motherboard: " -NoNewline
-Get-Mobo
-Write-Host "`n" -NoNewline
-
-#Define function to get GPU model
-function Get-GPU {
+function getGPU {
     $GPUbase = Get-WmiObject Win32_VideoController
     $GPUname = $GPUbase.Name
     $GPU= $GPUname + " at " + $GPUbase.CurrentHorizontalResolution + "x" + $GPUbase.CurrentVerticalResolution
-    return $GPU
+    $1 = "Graphics Card: " + $GPU
+	Return $1
 }
-Write-Host "Graphics Card: " -NoNewline
-Get-GPU
-Write-Host "`n" -NoNewline
-
-#Get list of installed updates
-Write-Host "Installed updates:"
-Get-HotFix |format-table -auto Description,HotFixID,InstalledOn
-Write-Host "`n" -NoNewLine
-
-#Get current users startup tasks and items
-function Get-Startup {
+function getRAM {
+	$1 = "`n" + "RAM: " + $(Get-WMIObject -class Win32_PhysicalMemory | Measure-Object -Property capacity -Sum | % {[Math]::Round(($_.sum / 1GB),2)}) + 'GB'
+	$2 = $(Get-WmiObject win32_physicalmemory | Format-Table Manufacturer,Configuredclockspeed,Devicelocator,Capacity,Serialnumber -autosize)
+	Return $1,$2
+}
+function getUpdates {
+	$1 = "`n" + "Installed updates:"
+	$2 = Get-HotFix |format-table -auto Description,HotFixID,InstalledOn
+	Return $1,$2
+}
+function getStartup {
     $startBase = Get-CimInstance Win32_StartupCommand
-    $startNames = $startBase.Caption
-    return $startNames
+    $1 = "Startup Tasks for user: "
+	$2 = $startBase.Caption
+	Return $1,$2
 }
-Write-Host "Startup Tasks for user: "
-Get-Startup
-Write-Host "`n" -NoNewline
-
-#Get current users running processes
-function Get-Processes {
+function getProcesses {
     $procBase = Get-Process
     $procTrash = $procBase.ProcessName
-    $procClean = $procTrash | select -Unique
-    return $procClean
+    $1 = "`n" + "Running processes: "
+	$2 = $($procTrash | select -Unique)
+	return $1,$2
 }
-Write-Host "Running processes: "
-Get-Processes
-Write-Host "`n" -NoNewline
-
-#Get system services and states
-Write-Host "Services: " -NoNewline
-Get-Service | Format-Table -auto
-Write-Host "`n" -NoNewline
-
-#Get Installed apps
-function Get-InstalledApplications {
+function getServices {
+	$1 = "`n" + "Services: "
+	$2 = $(Get-Service | Format-Table -auto)
+	Return $1,$2
+}
+function getInstalledApps {
     $installedBase = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*
-    $installedClean = $installedBase.DisplayName
-    return $installedClean
+    $1 = "Installed Apps: "
+	$2 = $installedBase.DisplayName
+	Return $1,$2
 }
-Write-Host "Installed Apps: "
-Get-InstalledApplications
-Write-Host "`n" -NoNewline
+function getDisks {
+	$1 = "`n" + "Disk layouts: "
+	$2 = $(Get-Partition|format-table -auto)
+	$3 = $(Get-Volume|format-table -auto)
+	Return $1,$2,$3
+}
+function getNets {
+	$1 = "`n" + "Network adapters:"
+	$2 = $(Get-NetAdapter|format-list Name,InterfaceDescription,Status,LinkSpeed)
+	$3 = $(Get-NetIPAddress|format-table -auto IpAddress,InterfaceAlias,PrefixOrigin)
+	Return $1,$2,$3
+}
+function getDrivers {
+	$1 = "`n" + "Drivers and device versions: "
+	$2 = $(gwmi Win32_PnPSignedDriver | format-table -auto devicename,driverversion)
+	Return $1,$2
+}
+function uploadFile {
+	$link = Invoke-WebRequest -ContentType 'text/plain' -Method 'PUT' -InFile $file -Uri "https://share.dev0.sh/upload/$null.txt" -UseBasicParsing
+	set-clipboard $link.Content
+}
+function promptUser {
+	. .\new-wpfMessageBox.ps1
+	$Params = @{
+		Content = "Do you want to view or upload the specs?"
+		Title = "rTechsupport "
+		TitleBackground = "DeepSkyBlue"
+		TitleFontSize = 28
+		TitleFontWeight = "Bold"
+		TitleTextForeground = "White"
+		ContentFontSize = 12
+		ContentFontWeight = "Medium"
+		ButtonType = "none"
+		CustomButtons = "View","Upload"
+	}
+	New-WPFMessageBox @Params
+	If ($WPFMessageBoxOutput -eq "View") {
+		Invoke-Item $file
+	}
+	ElseIf ($WPFMessageBoxOutput -eq "Upload") {
+		uploadFile
+		New-WPFMessageBox -Content "Link has been copied to your clipboard. Paste into chat to share." -Title "Upload Success" -TitleBackground Coral -WindowHost $Window
+	}
+}
 
-#Get disk health and layouts
-Write-Host "Disk layouts:"
-Get-Partition|format-table -auto
-Get-Volume|format-table -auto
-Write-Host "`n" -NoNewline
+# ------------------ #
+startItOut > $file
+basicInfo >> $file
+getCPU >> $file
+getMobo >> $file
+getGPU >> $file
+getRAM >> $file
+getUpdates >> $file
+getStartup >> $file
+getProcesses >> $file
+getServices >> $file
+getInstalledApps >> $file
+getDisks >> $file
+getNets >> $file
+getDrivers >> $file
+# ------------------ #
 
-#Get network adapter information
-Write-Host "Network adapters:"
-Get-NetAdapter|format-list Name,InterfaceDescription,Status,LinkSpeed
-Get-NetIPAddress|format-table -auto IpAddress,InterfaceAlias,PrefixOrigin
-Write-Host "`n" -NoNewline
-
-#Get drivers and devices
-Write-Host "Drivers and device versions:" -NoNewline
-gwmi Win32_PnPSignedDriver | format-table -auto devicename,driverversion
-
-Stop-transcript
-
-#Call the outputted file from above and then send it to pastebin server
-$FilePath = '.\TechSupport_Specs.txt'
-$link = Invoke-WebRequest -ContentType 'text/plain' -Method 'PUT' -InFile $FilePath -Uri "https://share.dev0.sh/upload/$null.txt" -UseBasicParsing
-
-#Write the link generated by the server to the clipboard and inform the user of how to use it
-# source pretty wpf box
-. .\new-wpfMessageBox.ps1
-set-clipboard $link.Content
-New-WPFMessageBox -Content "A link to the results is now on your clipboard. Paste into chat to share." -Title "rTechsupport Specs Tool" -TitleBackground Aquamarine
+# ------------------ #
+promptUser
+# ------------------ #
