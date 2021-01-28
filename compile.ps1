20f5a10c88f6b732e39f50016714176f195281d4
piper <piper@dev0.sh>
2021-01-28 18:50:04 -0500

    add

---
+++ compile.ps1
@@ -0,0 +1,1 @@
+ps2exe -inputFile .\WindowsSpecifications.ps1 -outputFile .\WindowsSpecifications.exe -x64 -noConsole -title 'rTechsupport Spec Tool' -company 'Dev0' -description 'Developed by PipeItToDevNull for rTechsupport'
