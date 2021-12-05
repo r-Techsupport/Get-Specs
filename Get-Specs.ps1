8dec8c9011e1b4b96d96d9ee120f26c04ed33150
PipeItToDevNull <piper@dev0.sh>
2021-12-04 21:02:48 -0500

    add bypass to fix #9

--- Get-Specs.ps1
+++ Get-Specs.ps1
@@ -18,6 +18,9 @@ Right click the zip file and choose 'Extract All' then run the new exe."
 }
 promptFileIssue
 
+# set execution policy to allow all our children to run
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
+
 # source our other ps1 files
 . files\wpf.ps1
 
