4ad30dfd2c4b82046fe496512470d0a80c3148a8
PipeItToDevNull <piper@dev0.sh>
2021-12-10 13:31:46 -0500

    add ico

--- compile.ps1
+++ compile.ps1
@@ -1,1 +1,1 @@
-ps2exe -inputFile .\Get-Specs.ps1 -outputFile .\Get-Specs.exe -x64 -title 'rTechsupport Spec Tool' -company 'Dev0' -description 'Developed by PipeItToDevNull for rTechsupport' -requireAdmin
+ps2exe -inputFile .\Get-Specs.ps1 -outputFile .\Get-Specs.exe -x64 -title 'rTechsupport Spec Tool' -company 'Dev0' -description 'Developed by PipeItToDevNull for rTechsupport' -requireAdmin -iconFile SNOO_256.ico
