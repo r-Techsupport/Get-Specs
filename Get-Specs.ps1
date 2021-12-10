cb83c21738d9c5318c996e54453e636046828d5a
PipeItToDevNull <piper@dev0.sh>
2021-12-10 12:10:45 -0500

    add force to alleviate Ajax issue with prompt

--- Get-Specs.ps1
+++ Get-Specs.ps1
@@ -19,7 +19,7 @@ Right click the zip file and choose 'Extract All' then run the new exe."
 promptFileIssue
 
 # set execution policy to allow all our children to run
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 
 # source our other ps1 files
 . files\wpf.ps1
