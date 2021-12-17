da3fb771ed0c3b0d3432b4b00376c002bd2ec9d6
PipeItToDevNull <piper@dev0.sh>
2021-12-17 12:50:12 -0500

    move to v 1.0.3

--- Get-Specs.ps1
+++ Get-Specs.ps1
@@ -19,7 +19,7 @@ Right click the zip file and choose 'Extract All' then run the new exe."
 promptFileIssue
 
 # VERSION
-$version = '1.0.2'
+$version = '1.0.3'
 
 # set execution policy to allow all our children to run
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
