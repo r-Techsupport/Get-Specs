436181640d517c6f8861808e53ee317f8d19ce8d
PipeItToDevNull <piper@dev0.sh>
2022-03-05 18:56:03 -0500

    add signature check on get-specs

--- loader.ps1
+++ loader.ps1
@@ -11,7 +11,19 @@ Right click the zip file and choose 'Extract All' then run the new exe."
             Throw
     }
 }
+function loadMain {
+    $validation = Get-AuthenticodeSignature .\Get-Specs.ps1
+    $cert = $validation | Select -ExpandProperty SignerCertificate
+    If ($validation.Status -ne 'Valid') {
+        msg.exe * "Get-Specs.ps1 has an invalid signature"
+        Throw
+    }
+    If ($cert.Thumbprint -ne 'ACD87F7650858831D787D734D635CC32705166D2') {
+        msg.exe * "Get-Specs.ps1 has an invalid signature"
+        Throw
+    } Else {
+        ./Get-Specs.ps1
+    }
+}
 promptFileIssue
-
-# Chainload our primary script
-./Get-Specs.ps1
+loadMain
