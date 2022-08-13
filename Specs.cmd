b521294eb3d4a6d2305774357deea060c590d302
SealsRock12 <sign@toad39.dev>
2022-08-12 20:39:42 -0700

    Escape dir name

--- Specs.cmd
+++ Specs.cmd
@@ -1,2 +1,2 @@
-powershell -Command "Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass; Start-Process PowerShell -Verb RunAs -ArgumentList ('-NoExit',('cd {0};' -f $pwd.ProviderPath),'.\Get-Specs.ps1')"
+powershell -Command "Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass; Start-Process PowerShell -Verb RunAs -ArgumentList ('-NoExit',('cd ''{0}'';' -f $pwd.ProviderPath),'.\Get-Specs.ps1')"
 
