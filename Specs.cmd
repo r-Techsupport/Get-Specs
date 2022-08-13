9a287a4950508c3180c7c49e0faedf4698d5cab8
PipeItToDevNull <piper@dev0.sh>
2022-08-13 14:45:51 -0400

    add files check

--- Specs.cmd
+++ Specs.cmd
@@ -1,2 +1,7 @@
+IF NOT EXIST ".\files" (
+    msg.exe * "The files directory is missing. This most likely means you did not extract the zip file properly. Right click the zip file and choose 'Extract All' then run the new exe."
+    exit
+)
+
 powershell Start-Process PowerShell -Verb RunAs -ArgumentList ('Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass; ',('cd ''{0}'';' -f $pwd.ProviderPath),'.\Get-Specs.ps1')
 
